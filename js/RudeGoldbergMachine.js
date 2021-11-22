8c182dbaa3bd55c9e49f0153fc10bf28729118ad
Halil Ibrahim Keser <hke006@uit.no>
2021-11-22 17:23:09 +0100

    Startet på TweenElevator.js

--- js/RudeGoldbergMachine.js
+++ js/RudeGoldbergMachine.js
@@ -2,7 +2,7 @@
  * Bruker et eksempel fra faglærer Werner som startpunkt skybox2.js
  * For terrreng har vi hentet inn kode fra terreng2.js, deler av det er så flyttet til egen js fil.
  * MyThreeScene kommer fra lib mappen. Den er gjort noen få endringer på.
- *
+ * Tween er implementert i TweenElevator.js, kode hentet fra Tween1.js
  *
  *
  */
@@ -22,6 +22,8 @@ import {myThreeScene} from "../lib/threehelpers/MyThreeScene.js";
 import {sunGear} from "./SunGear.js";
 import {verticalChain} from "./VerticalChain.js";
 import {startGearHolder} from "./StartGearHolder.js";
+import {tweenElevator} from "./TweenElevator.js";
+import {TWEEN} from "../lib/three/examples/jsm/libs/tween.module.min.js";
 
 let renderer;
 let scene;
@@ -40,6 +42,18 @@ export function main() {
 	// SKYBOX
 	addSkybox();
 
+	addModels();
+
+    // RESIZE
+    window.addEventListener('resize', onWindowResize, false);
+    document.addEventListener('keyup', handleKeyUp, false);
+    document.addEventListener('keydown', handleKeyDown, false);
+
+	animate();
+}
+
+
+function addModels() {
 	// SUNGEAR
 	sunGear.init(myThreeScene.scene);
 	sunGear.create();
@@ -52,13 +66,11 @@ export function main() {
 	startGearHolder.init(myThreeScene.scene);
 	startGearHolder.create();
 
-    // RESIZE
-    window.addEventListener('resize', onWindowResize, false);
-    document.addEventListener('keyup', handleKeyUp, false);
-    document.addEventListener('keydown', handleKeyDown, false);
-
-	animate();
+	//elevator
+	// tweenElevator.init(myThreeScene.scene);
+	// tweenElevator.create();
 }
+
 
 function handleKeyUp(event) {
     currentlyPressedKeys[event.keyCode] = false;
@@ -68,9 +80,11 @@ function handleKeyDown(event) {
     currentlyPressedKeys[event.keyCode] = true;
 }
 
-function animate(currentTime) {
+export function animate(currentTime) {
     requestAnimationFrame(animate);
     let delta = clock.getDelta();
+
+	TWEEN.update(currentTime);
 
 	//collisionTest();
 	myThreeScene.updateGraphics(delta);
---
+++ js/TweenElevator.js
@@ -0,0 +1,66 @@
+import * as THREE from "../lib/three/build/three.module.js";
+import {commons} from "../lib/ammohelpers/lib/Common.js";
+import {TWEEN} from "../lib/three/examples/jsm/libs/tween.module.min.js";
+
+export const tweenElevator = {
+    myPhysicsWorld: undefined,
+    scene: undefined,
+
+    init(scene) { // må endres til ammophysicsworld
+        this.scene = scene;
+    },
+
+    create(setCollisionMask = true, mass = 0, texture = false, color = 0xF4F0EF, position = {
+        x: -360,
+        y: -75,
+        z: -8
+    }, radius = 0.2, length = 350, width = 150) {
+        if (texture) { //Hvis tekstur er ønsket
+            const loadManager = new THREE.LoadingManager();
+            const loader = new THREE.TextureLoader(loadManager);
+            this.material = new THREE.MeshPhongMaterial({map: loader.load(url)});
+        } else {
+            this.material = new THREE.MeshPhongMaterial({color: color});
+        }
+
+        let groupMesh = new THREE.Group();
+        groupMesh.userData.tag = "rudegoldberg";
+        groupMesh.userData.name = "tweenelevator"
+        groupMesh.position.z = -500;
+        this.scene.add(groupMesh);
+
+        //let elevator = new THREE.Mesh(this.createHoledCylinderShape(), this.material);
+
+        // https://github.com/tweenjs/tween.js/blob/master/docs/user_guide.md
+        tween = new TWEEN.Tween({y: 0, x: 0})
+            .to({y: 180, x: 200}, 5000)
+            .easing(TWEEN.Easing.Bounce.InOut)
+            .yoyo(true)
+            .repeat(Infinity)
+            .onUpdate(this.animateModel);
+
+        tween.start();
+
+        this.animate();
+    },
+
+    animateModel(position) {
+        // Bruk y'en til noe...:
+        let lumaModel = scene.getObjectByName('tweenelevator', true);
+        if (lumaModel) {
+            lumaModel.position.set(position.x, position.y, 0);
+        }
+    },
+
+
+    //
+    // animate(currentTime) {
+    //     requestAnimationFrame(animate);
+    //     //Oppdaterer tween:
+    //     TWEEN.update(currentTime);
+    //
+    //     renderer.render(scene, camera);
+    // }
+
+
+}
